77482619a1a791e8984f8d590e7aed1c6d4548c7
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-26 21:01:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
-for (let i = 0; i < 25; i++) {
-  var array = []
-  if (i = 1) {
-    array.push("I am 1 strange loop.")
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i = 1) {
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops`)
+    }
+    return array
   }
-  else {
-    array.push(`I am ${i} strange loops`)
-  }
-  return array
 }
